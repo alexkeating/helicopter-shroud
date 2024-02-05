ec50f05240d9dae5ec25169e7e56e6c2fff1884d
keating <keating.dev@protonmail.com>
2024-02-04 22:03:37 -0500

    Working frontend

--- apps/helicopter-shroud/src/app/App.tsx
+++ apps/helicopter-shroud/src/app/App.tsx
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
+import NxWelcome from './NxWelcome';
 
 export function App() {
   return (
